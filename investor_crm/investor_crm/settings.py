86c06b6510d7d53dc9f150b19783052b6cefc397
RoyKeane94 <thomasbarratt@yahoo.com>
2026-03-10 21:07:30 +0000

    Fix static files: absolute STATIC_URL and simpler WhiteNoise storage

--- investor_crm/investor_crm/settings.py
+++ investor_crm/investor_crm/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
