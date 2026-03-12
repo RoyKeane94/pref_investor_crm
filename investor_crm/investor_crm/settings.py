fd7fa6020e2011940058bdcb590ed326cca7203b
RoyKeane94 <thomasbarratt@yahoo.com>
2026-03-12 21:43:25 +0000

    Debug permanently set to False

--- investor_crm/investor_crm/settings.py
+++ investor_crm/investor_crm/settings.py
@@ -28,7 +28,7 @@ if (env_file := BASE_DIR / '.env').exists():
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split() or []
 
